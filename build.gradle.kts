e21e99fa1d348fc777b7ffb8db39db0edd329eba
Jakub Petrzilka <jakub.petrzilka@dossier.no>
2021-11-26 11:34:19 +0100

    Upgrading kotlinx serialization dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-version = "0.16"
+version = "0.17"
 
 plugins {
     kotlin("plugin.serialization") version "1.5.21"
@@ -11,7 +11,7 @@ kotlin {
             dependencies {
                 implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
                 implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.5.0")
                 implementation("com.rabbitmq:amqp-client:5.14.0")
