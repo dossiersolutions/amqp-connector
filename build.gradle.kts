686fd07a4387c87d2d158eb9a24eff0a9b81a740
Jakub Petrzilka <jakub.petrzilka@dossier.no>
2024-01-03 10:27:02 +0100

    PF-21247 bumping version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import java.net.URL
 import java.util.Base64
 
 group = "no.dossier.libraries"
-version = "0.2.3"
+version = "0.2.4"
 
 object Meta {
     const val desc = "RabbitMQ Kotlin Client library"
